42040380944e69548538cd83dee0529fab3b0b99
Juvin <hdcqW@outlook.com>
2025-12-25 11:25:59 +0800

    更改文件结构，创建数据库连接池ConnectionPool!

---
+++ src/utils/ConnectionPool.cpp
@@ -0,0 +1,31 @@
+#include<QSqlDatabase>
+#include<QThread>
+#include "ConnectionPool.h"
+
+QSqlDatabase ConnectionPool::getThreadLocalConnection(){
+    QString connectionName=QString("Conn_%1").arg((quint64)QThread::currentThreadId());
+    if(QSqlDatabase::contains(connectionName)){
+        return QSqlDatabase::database(connectionName);
+    }
+    else{
+        QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL",connectionName);
+        db.setHostName("127.0.0.1");
+        db.setPort(3306);
+        db.setDatabaseName("rainhub_db"); //数据库名
+        db.setUserName("root"); //用户名
+        db.setPassword("root"); //密码
+        if(!db.open()){
+            qCritical()<<"Failed to connect to database: "<<db.lastError().text();
+        }else{
+            qInfo()<<"Connected to database: "<<db.databaseName();
+        }
+        return db;
+    }
+}
+
+void ConnectionPool::removeThreadConnection(){
+    QString connectionName=QString("Conn_%1").arg((quint64)QThread::currentThreadId());
+    if(QSqlDatabase::contains(connectionName)){
+        QSqlDatabase::removeDatabase(connectionName);
+    }
+}
---
+++ src/utils/ConnectionPool.h
@@ -0,0 +1,17 @@
+/*
+数据库连接池类，用于管理数据库连接，避免频繁创建和销毁连接，提高性能。
+这里用的是单例模式，也就是只有一个实例，所有的线程都共享这个实例。
+ */
+
+#pragma once
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
+#include<QThread>
+
+class ConnectionPool{
+    public:
+        static QSqlDatabase getThreadLocalConnection(); //获取线程本地连接
+        static void removeThreadConnection();  //移除线程本地连接
+}
