b43308bd52b021a7b8d7f86be8369675c72f7699
Juvin <hdcqW@outlook.com>
2025-12-27 17:47:47 +0800

    添加站点的查询服务（为了绘制map）

---
+++ src/control/StationService.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<QVector>
+#include<memory>
+#include"../dao/StationDao.h"
+#include"../model/Stationlocal.h"
+
+class StationService {
+public:
+    //获取所有站点信息
+    QVector<std::unique_ptr<Stationlocal>> getAllStations();
+    //获取单个站点详情
+    std::unique_ptr<Stationlocal> getStationDetail(Station stationId);
+private:
+    StationDao stationDao;
+};
