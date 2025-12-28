f41d7f2103113423605caac28287ab8dbda8a246
Juvin <hdcqW@outlook.com>
2025-12-29 01:29:47 +0800

    将原来AdminService.cpp解耦，单放管理员认证服务

---
+++ src/control/Admin_AuthService.cpp
@@ -0,0 +1,23 @@
+/*
+    管理员认证服务实现
+*/
+#include "Admin_AuthService.h"
+#include "../utils/ConnectionPool.h"
+
+#include <QDebug>
+
+std::optional<User> Admin_AuthService::adminLogin(const QString& userId, const QString& password) {
+    QSqlDatabase db = ConnectionPool::getThreadLocalConnection();
+    if (!db.isOpen()) {
+        qCritical() << "[Admin_AuthService] 数据库连接失败";
+        return std::nullopt;
+    }
+    
+    auto userOpt = userDao.selectById(db, userId);
+    if (!userOpt) { return std::nullopt;  }
+    
+    if (userOpt->get_password() != password) {return std::nullopt; } //密码错误
+    if (userOpt->get_role() != 9) {return std::nullopt; } //不是管理员
+    
+    return userOpt;
+}
---
+++ src/control/Admin_AuthService.h
@@ -0,0 +1,18 @@
+/*
+    管理员认证服务
+*/
+#pragma once
+
+#include <QString>
+#include <optional>
+
+#include "../dao/UserDao.h"
+#include "../model/User.h"
+
+class Admin_AuthService {
+public:
+    //管理员登录验证
+    std::optional<User> adminLogin(const QString& userId, const QString& password);
+private:
+    UserDao userDao;
+};
